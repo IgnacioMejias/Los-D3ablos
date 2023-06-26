38cb02a6853baab75979729d2d14cfa5240c2f55
Juan Fernandez <jfernaandez@uc.cl>
2023-06-26 03:16:09 -0400

    Mapa Puntos funcionando, ahora asistencias y rebotes

--- visualization/main.js
+++ visualization/main.js
@@ -1,59 +1,164 @@
-d3.json("../dataset/usa.json").then((datosTopo) => {
-    const datos = topojson.feature(datosTopo, datosTopo.objects.states);
+// Cargamos los datos de estados y equipos
+d3.json("../US_States_and_Teams.json").then((dataUSAEquipos) => {
+  // Cargamos los datos de los equipos
+  d3.json("../team_points_per_season.json").then((dataEquipos) => {
+    d3.json("../dataset/usa.json").then((datosTopo) => {
+      const datos = topojson.feature(datosTopo, datosTopo.objects.states);
 
-    // Define el tamaño del SVG
-    const width = 960;
-    const height = 600;
+      // Define el tamaño del SVG
+      const width = 960;
+      const height = 600;
 
-    // Crea el SVG
-    const svg = d3.select("body").append("svg")
+      // Crea el SVG
+      const svg = d3
+        .select("body")
+        .append("svg")
         .attr("width", width)
         .attr("height", height);
 
-    // Define la proyección
-    const projection = d3.geoAlbersUsa()
+      // Define la escala de color con valores iniciales
+      let minPuntos = 0;
+      let maxPuntos = 10000; // Valores predeterminados
+
+      // Función para calcular el valor mínimo y máximo de los puntos por temporada
+      function calcularDominio(año) {
+        const puntosPorTemporada = Object.values(dataEquipos).map(
+          (equipo) => equipo[año]
+        );
+        minPuntos = d3.min(puntosPorTemporada);
+        maxPuntos = d3.max(puntosPorTemporada);
+        console.log(minPuntos, maxPuntos);
+      }
+
+      // Define la escala de color
+      const colorScale = d3
+        .scaleSequential()
+        .interpolator(d3.interpolateOrRd) // Utiliza el esquema de colores rojo-azul
+        .domain([7476, 8649]); // Rango de puntos por temporada
+
+      // Define la proyección
+      const projection = d3
+        .geoAlbersUsa()
         .translate([width / 2, height / 2]) // centra el mapa
         .scale(width); // escala el mapa
 
-    // Define el generador de caminos
-    const path = d3.geoPath().projection(projection);
+      // Define el generador de caminos
+      const path = d3.geoPath().projection(projection);
 
-    // Dibuja cada estado usando los datos
-    svg.selectAll("path")
+      // Función para actualizar el mapa en función del año seleccionado
+      function actualizarMapa(año, opcion) {
+        // Calcula el dominio de los puntos por temporada
+        calcularDominio(año);
+        colorScale.domain([minPuntos, maxPuntos]);
+
+        svg
+          .selectAll("path")
+          .data(datos.features)
+          .style("fill", function (d) {
+            let equipoMaxPuntos = null;
+            let maxPuntos = 0;
+
+            const nombreEstado = d.properties.name;
+            const equipos = dataUSAEquipos[nombreEstado];
+            if (equipos && equipos.length > 0) {
+              // Busca el equipo con más puntos en el año seleccionado
+              equipos.forEach((equipo) => {
+                const puntos = dataEquipos[equipo][año];
+                if (puntos && puntos > maxPuntos) {
+                  equipoMaxPuntos = equipo;
+                  maxPuntos = puntos;
+                }
+              });
+
+              if (equipoMaxPuntos) {
+                const puntos = dataEquipos[equipoMaxPuntos][año];
+                return colorScale(puntos);
+              }
+            }
+            return "rgb(173,216,230)";
+          });
+      }
+
+      // Genera las opciones del dropdown para los años desde 1980 hasta 2022
+      const añoSelect = document.getElementById("año-select");
+      for (let año = 2022; año >= 1980; año--) {
+        const option = document.createElement("option");
+        option.value = año;
+        option.text = año;
+        añoSelect.appendChild(option);
+      }
+
+      // Llamada inicial para mostrar los datos del año 2022
+      actualizarMapa("2022", "puntos");
+
+      añoSelect.addEventListener("change", function () {
+        const añoSeleccionado = añoSelect.value;
+        actualizarMapa(añoSeleccionado, "puntos");
+      });
+
+      // Dibuja cada estado usando los datos
+      svg
+        .selectAll("path")
         .data(datos.features)
         .enter()
         .append("path")
         .attr("d", path)
         .style("stroke", "#fff") // color de las líneas
         .style("stroke-width", "1") // grosor de las líneas
-        .style("fill", "rgb(173,216,230)") // color de relleno
-        .on("mouseover", function(event, d) { // evento de mouseover
-                // Muestra el nombre del estado en el mapa
-                svg.append("text")
-                    .attr("x", 600)
-                    .attr("y", 100)
-                    .text(d.properties.name)
-                    .attr("font-family", "sans-serif")
-                    .attr("font-size", "20px")
-                    .attr("fill", "black");
+        .on("mouseover", function (event, d) {
+          // evento de mouseover
+          // Muestra el nombre del estado en el mapa
+          svg
+            .append("text")
+            .attr("x", 600)
+            .attr("y", 100)
+            .text(d.properties.name)
+            .attr("font-family", "sans-serif")
+            .attr("font-size", "20px")
+            .attr("fill", "black");
         })
-        .on("mouseout", function(event, d) { // evento de mouseout
+        .on("mouseout", function (event, d) {
+          // evento de mouseout
+          // Elimina el texto
+          svg.select("text").remove();
+        })
+        .on("click", function (event, d) {
+          // evento de click
+          // Verifica el color actual del estado
+          const añoActual = añoSelect.value;
+          const currentColor = d3.select(this).style("fill");
+          if (currentColor === "rgb(65, 105, 225)") {
+            // Si el estado ya está seleccionado, cambia su color de vuelta al color original
+            d3.select(this).style("fill", function (d) {
+              let equipoMaxPuntos = null;
+              let maxPuntos = 0;
+
+              const nombreEstado = d.properties.name;
+              const equipos = dataUSAEquipos[nombreEstado];
+              if (equipos && equipos.length > 0) {
+                // Busca el equipo con más puntos en el año seleccionado
+                equipos.forEach((equipo) => {
+                  const puntos = dataEquipos[equipo][añoActual];
+                  if (puntos > maxPuntos) {
+                    equipoMaxPuntos = equipo;
+                    maxPuntos = puntos;
+                  }
+                });
+
+                const puntos = dataEquipos[equipoMaxPuntos][añoActual];
+                return colorScale(puntos);
+              }
+              return "rgb(173,216,230)";
+            });
+
             // Elimina el texto
             svg.select("text").remove();
-        })
-        .on("click", function(event, d) { // evento de click
-            // Verifica el color actual del estado
-            const currentColor = d3.select(this).style("fill");
-            if (currentColor === "rgb(255, 0, 0)") {
-                // Si el estado ya está seleccionado, cambia su color de vuelta al color original
-                d3.select(this).style("fill", "rgb(173,216,230)");
-
-                // Elimina el texto
-                svg.select("text").remove();
-            } else {
-                // Si el estado no está seleccionado, cambia su color a rojo
-                d3.select(this).style("fill", "rgb(255,0,0)");
-
-            }
+          } else {
+            // Si el estado no está seleccionado, cambia su color a rojo
+            d3.select(this).style("fill", "rgb(65,105,225)");
+          }
         });
+      actualizarMapa("2022", "puntos");
+    });
+  });
 });
--- visualization/multilineChart.js
+++ visualization/multilineChart.js
@@ -1,119 +1,190 @@
 // cargar los datos desde el archivo JSON
-d3.json("players_data.json").then(function(data) {
-
-    // obtener todas las temporadas
-    let allSeasons = [];
-    for (let player in data) {
-        for (let season in data[player]) {
-            if (!allSeasons.includes(season)) {
-                allSeasons.push(season);
-            }
-        }
+d3.json("../players_data.json").then(function (data) {
+  // obtener todas las temporadas
+  let allSeasons = [];
+  for (let player in data) {
+    for (let season in data[player]) {
+      if (!allSeasons.includes(season)) {
+        allSeasons.push(season);
+      }
     }
-    allSeasons.sort();
+  }
+  allSeasons.sort();
 
-    // procesar los datos
-    let processedData = [];
-    for (let player in data) {
-        let playerData = {
-            name: player,
-            values: []
-        };
-        let totalPoints = 0; // contador acumulativo de puntos
-        for (let season of allSeasons) {
-            // asegurarse de que cada jugador tenga un registro para cada temporada
-            if (data[player][season] !== undefined) {
-                totalPoints += data[player][season];
-            }
-            playerData.values.push({
-                season: season,
-                points: totalPoints
-            });
-        }
-        processedData.push(playerData);
+  // procesar los datos
+  let processedData = [];
+  for (let player in data) {
+    let playerData = {
+      name: player,
+      values: [],
+    };
+    let totalPoints = 0;
+    for (let season of allSeasons) {
+      if (data[player][season] !== undefined) {
+        totalPoints += data[player][season];
+      }
+      playerData.values.push({
+        season: season,
+        points: totalPoints,
+      });
     }
+    processedData.push(playerData);
+  }
 
-    // establecer los márgenes y las dimensiones del gráfico
-    let margin = {top: 20, right: 80, bottom: 30, left: 50};
-    let width = 960 - margin.left - margin.right;
-    let height = 500 - margin.top - margin.bottom;
+  // establecer los márgenes y las dimensiones del gráfico
+  let margin = { top: 20, right: 80, bottom: 30, left: 50 };
+  let width = 960 - margin.left - margin.right;
+  let height = 500 - margin.top - margin.bottom;
 
-    // configurar las escalas x e y
-    let x = d3.scaleBand().range([0, width]).padding(0.1);
-    let y = d3.scaleLinear().range([height, 0]);
-
-    // configurar el generador de líneas
-    let line = d3.line()
-        .x(function(d) { return x(d.season) + x.bandwidth() / 2; }) // coloca cada punto en el centro de su banda
-        .y(function(d) { return y(d.points); });
-
-    // agregar un SVG al cuerpo del documento
-    let svg = d3.select("#multilineChart").append("svg")
-        .attr("width", width + margin.left + margin.right)
-        .attr("height", height + margin.top + margin.bottom)
-        .append("g")
-        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-    // definir el dominio para las escalas x e y
-    x.domain(allSeasons);
-    y.domain([
-        0,
-        d3.max(processedData, function(c) { return d3.max(c.values, function(d) { return d.points; }); })
+  // configurar las escalas x e y
+  let x = d3.scaleBand().range([0, width]).padding(0.1);
+  let y = d3
+    .scaleLinear()
+    .range([height, 0])
+    .domain([
+      0,
+      d3.max(processedData.flatMap((d) => d.values.map((p) => p.points))),
     ]);
 
-    // agregar el eje X
-    svg.append("g")
-        .attr("transform", "translate(0," + height + ")")
-        .call(d3.axisBottom(x))
-        .selectAll("text")
-        .style("text-anchor", "end")
-        .attr("dx", "-.8em")
-        .attr("dy", ".15em")
-        .attr("transform", "rotate(-65)");
+  // configurar el generador de líneas
+  let line = d3
+    .line()
+    .x(function (d) {
+      return x(d.season) + x.bandwidth() / 2;
+    })
+    .y(function (d) {
+      return y(d.points);
+    });
 
-    // agregar el eje Y
-    svg.append("g")
-        .call(d3.axisLeft(y));
+  // agregar un SVG al cuerpo del documento
+  let svg = d3
+    .select("#multilineChart")
+    .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-// dibujar las líneas
-processedData.forEach(function(playerData, i) {
-    let path = svg.append("path")
-        .datum(playerData.values)
-        .attr("fill", "none")
-        .attr("stroke", function() { return d3.schemeCategory10[i % 10]; }) // alternar colores
-        .attr("stroke-linejoin", "round")
-        .attr("stroke-linecap", "round")
-        .attr("stroke-width", 1.5)
-        .attr("d", line);
+  // definir el dominio para las escalas x e y
+  x.domain(allSeasons);
+  y.domain([
+    0,
+    d3.max(processedData, function (c) {
+      return d3.max(c.values, function (d) {
+        return d.points;
+      });
+    }),
+  ]);
 
-    // agregar animación
+  // agregar el eje X
+  svg
+    .append("g")
+    .attr("class", "xaxis")
+    .attr("transform", "translate(0," + height + ")")
+    .call(d3.axisBottom(x))
+    .selectAll("text")
+    .style("text-anchor", "end")
+    .attr("dx", "-.8em")
+    .attr("dy", ".15em")
+    .attr("transform", "rotate(-65)");
+
+  // agregar el eje Y
+  svg.append("g").attr("class", "yaxis").call(d3.axisLeft(y));
+
+  const circle = svg
+    .append("circle")
+    .attr("r", 50)
+    .attr("fill", "orange")
+    .attr("stroke", "black")
+    .attr("cx", 200)
+    .attr("cy", 100);
+
+  // Función encargada de manejar el zoom.
+const manejadorZoom = (evento) => {
+    const transformacion = evento.transform;
+  
+    // Actualizar la escala y en función de la transformación del zoom
+    let newY = transformacion.rescaleY(y);
+  
+    // Actualizar el eje y con la nueva escala
+    svg.select(".yaxis").call(d3.axisLeft(newY));
+  
+    // Actualizar las líneas con la nueva escala
+    svg.selectAll(".line")
+      .attr("d", function (d) {
+        return line(d.values);
+      });
+  
+    // Actualizar las posiciones de los elementos legend
+    svg.selectAll("text")
+      .attr("transform", function (d) {
+        if (d.values && Array.isArray(d.values)) {
+        let seasonIndex = Math.round(d3.interpolateNumber(0, d.values.length - 1)(transformacion.k));
+        return "translate(" + x(d.values[seasonIndex].season) + "," + newY(d.values[seasonIndex].points) + ")";
+        } else {
+            console.log(d);
+        }
+      });
+  };
+  
+
+  const zoom = d3
+    .zoom()
+    .scaleExtent([0.5, 2])
+    .on("zoom", manejadorZoom);
+
+  svg.call(zoom);
+
+  // dibujar las líneas
+  processedData.forEach(function (playerData, i) {
+    let path = svg
+      .append("path")
+      .datum(playerData.values)
+      .attr("class", "line") // agregar la clase "line" a las líneas
+      .attr("fill", "none")
+      .attr("stroke", function () {
+        return d3.schemeCategory10[i % 10];
+      })
+      .attr("stroke-linejoin", "round")
+      .attr("stroke-linecap", "round")
+      .attr("stroke-width", 1.5)
+      .attr("d", line);
+
     let totalLength = path.node().getTotalLength();
-    path.attr("stroke-dasharray", totalLength + " " + totalLength)
-        .attr("stroke-dashoffset", totalLength)
-        .transition()
-        .duration(10000)
-        .ease(d3.easeLinear)
-        .attr("stroke-dashoffset", 0);
+    path
+      .attr("stroke-dasharray", totalLength + " " + totalLength)
+      .attr("stroke-dashoffset", totalLength)
+      .transition()
+      .duration(1000)
+      .ease(d3.easeLinear)
+      .attr("stroke-dashoffset", 0);
 
-    // agregar leyenda
-    let legend = svg.append("text")
-        .datum({name: playerData.name, value: playerData.values[0]})
-        .attr("x", 3)
-        .attr("dy", "0.35em")
-        .style("font", "10px sans-serif")
-        .text(function(d) { return d.name; });
+    let legend = svg
+      .append("text")
+      .datum({ name: playerData.name, value: playerData.values[0] })
+      .attr("x", 3)
+      .attr("dy", "0.35em")
+      .style("font", "10px sans-serif")
+      .text(function (d) {
+        return d.name;
+      });
 
-    // agregar animación a la leyenda
-    legend.transition()
-        .duration(10000)
-        .ease(d3.easeLinear)
-        .attrTween("transform", function(d) {
-            let interpolate = d3.interpolateNumber(0, playerData.values.length - 1);
-            return function(t) {
-                let seasonIndex = Math.round(interpolate(t));
-                return "translate(" + x(playerData.values[seasonIndex].season) + "," + y(playerData.values[seasonIndex].points) + ")";
-            };
-        });
-});
-
+    legend
+      .transition()
+      .duration(1000)
+      .ease(d3.easeLinear)
+      .attrTween("transform", function (d) {
+        let interpolate = d3.interpolateNumber(0, playerData.values.length - 1);
+        return function (t) {
+          let seasonIndex = Math.round(interpolate(t));
+          return (
+            "translate(" +
+            x(playerData.values[seasonIndex].season) +
+            "," +
+            y(playerData.values[seasonIndex].points) +
+            ")"
+          );
+        };
+      });
+  });
 });
